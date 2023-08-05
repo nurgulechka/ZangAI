bde87f520e369704d4910150170a9a5d3e3218a5
nurgul <nu.kozhakhmetova@gmail.com>
2023-08-05 18:40:32 +0600

    Initial Commit

---
+++ ZangAI/ZangAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZangAIApp.swift
+//  ZangAI
+//
+//  Created by Nurgul Kozhakhmetova on 05.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct ZangAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
